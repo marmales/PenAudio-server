e0a59713661fd5712e55f1703198c6fbf6212522
mmaleszewski <marcin.maleszewski@accenture.com>
2019-03-09 13:31:06 +0100

    Initial commit

---
+++ ServerPenAudio/Controllers/AudioController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServerPenAudio.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AudioController : ControllerBase
+	{
+		[HttpPost]
+		public ActionResult Upload(string[] data)
+		{
+			return new JsonResult("ok");
+		}
+	}
+}
